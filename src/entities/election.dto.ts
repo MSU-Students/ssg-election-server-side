515f89fd16a098699cdbf4d8a4a3a91b0357906f
Najmah Omar <najmahbusran@gmail.com>
2022-03-09 10:17:37 +0800

    election update

--- src/entities/election.dto.ts
+++ src/entities/election.dto.ts
@@ -11,5 +11,22 @@ import { Election } from 'src/interfaces/election.interface';
 
 @Entity('election')
 export class ElectionDto implements Election {
+  @PrimaryGeneratedColumn()
+  election_id?: number;
 
+  @ApiProperty()
+  @Column({ type: 'int' })
+  election_sem: number;
+
+  @ApiProperty({ example: '2020' })
+  @Column({ type: 'int' })
+  election_year: number;
+
+  @ApiProperty({ example: '2021' })
+  @Column({ type: 'date' })
+  election_date: number;
+
+  @ApiProperty({ example: '8' })
+  @Column({ type: 'time' })
+  election_time: number;
 }
