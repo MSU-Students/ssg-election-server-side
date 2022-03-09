45237ec908dcd3336a8a62c8585d8ff6cdfba8da
Najmah Omar <najmahbusran@gmail.com>
2022-03-09 18:58:52 +0800

    student s co

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { StudentDto } from 'src/entities/student.dto';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(StudentDto)
+    private studentRepository: Repository<StudentDto>,
+  ) {}
+
+  async create(application: StudentDto): Promise<StudentDto> {
+    return this.studentRepository.save(application);
+  }
+  async findAll(): Promise<StudentDto[]> {
+    return this.studentRepository.find();
+  }
+  async findOne(student_id: number): Promise<StudentDto> {
+    return this.studentRepository.findOne(student_id);
+  }
+  async update(student_id: number, application: StudentDto) {
+    return this.studentRepository.update(student_id, application);
+  }
+  async deleteOne(student_id: number) {
+    return this.studentRepository.delete(student_id);
+  }
+}
