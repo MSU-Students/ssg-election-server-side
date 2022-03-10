528696c07b39ea85f7ae0f4757e6c7968b94956f
Najmah Omar <najmahbusran@gmail.com>
2022-03-10 14:33:45 +0800

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,24 @@ import { AccountTypeController } from './account-type/account-type.controller';
 import { AccountTypeService } from './account-type/account-type.service';
 import { UserDto } from './user';
 import { AuthModule } from './user/auth.module';
+import { StudentDto } from './entities/student.dto';
+import { CandidateDto } from './entities/candidate.dto';
+import { ElectionDto } from './entities/election.dto';
+import { TempTallyDto } from './entities/temp-tally.dto';
+import { PositionsDto } from './entities/positions.dto';
+import { StudentAcademicYrDto } from './entities/student-academic-yr.dto';
+import { StudentController } from './student/student.controller';
+import { CandidateController } from './candidate/candidate.controller';
+import { ElectionController } from './election/election.controller';
+import { TempTallyController } from './temp-tally/temp-tally.controller';
+import { PositionsController } from './positions/positions.controller';
+import { StudentAcademicYrController } from './student-academic-yr/student-academic-yr.controller';
+import { StudentService } from './student/student.service';
+import { CandidateService } from './candidate/candidate.service';
+import { ElectionService } from './election/election.service';
+import { TempTallyService } from './temp-tally/temp-tally.service';
+import { PositionsService } from './positions/positions.service';
+import { StudentAcademicYrService } from './student-academic-yr/student-academic-yr.service';
 
 @Module({
   imports: [
@@ -29,6 +47,12 @@ import { AuthModule } from './user/auth.module';
       AdminDto,
       PrimeMinisterDto,
       AccountTypeDto,
+      StudentDto,
+      CandidateDto,
+      ElectionDto,
+      TempTallyDto,
+      PositionsDto,
+      StudentAcademicYrDto,
     ]),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -44,6 +68,12 @@ import { AuthModule } from './user/auth.module';
         AdminDto,
         PrimeMinisterDto,
         AccountTypeDto,
+        StudentDto,
+        CandidateDto,
+        ElectionDto,
+        TempTallyDto,
+        PositionsDto,
+        StudentAcademicYrDto,
       ],
       synchronize: true,
       dropSchema: true,
@@ -57,6 +87,12 @@ import { AuthModule } from './user/auth.module';
     AdminController,
     PrimeMinisterController,
     AccountTypeController,
+    StudentController,
+    CandidateController,
+    ElectionController,
+    TempTallyController,
+    PositionsController,
+    StudentAcademicYrController,
   ],
   providers: [
     UserService,
@@ -65,6 +101,12 @@ import { AuthModule } from './user/auth.module';
     AdminService,
     PrimeMinisterService,
     AccountTypeService,
+    StudentService,
+    CandidateService,
+    ElectionService,
+    TempTallyService,
+    PositionsService,
+    StudentAcademicYrService,
   ],
 })
 export class AppModule {}
